2aba374926a2921c732b545880f522c043a805e3
Othmaneelmo <oelmo031@uottawa.ca>
2025-05-05 07:40:19 -0400

    move terminal commands from init.lua to vim-options.lua

--- init.lua
+++ init.lua
@@ -22,18 +22,4 @@ vim.g.maplocalleader = "\\"
 require("vim-options")
 require("lazy").setup("plugins")
 
--- Exit terminal mode with Esc
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
-
--- Move around from terminal using Ctrl + h/j/k/l
-vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-w>h]])
-vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-w>j]])
-vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-w>k]])
-vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-w>l]])
-
--- Open terminal in horizontal split with Ctrl-t
-vim.keymap.set('n', '<C-t>', ':split | terminal<CR>', { noremap = true, silent = true })
-
--- Open terminal in vertical split with Ctrl-v, then t (C-vt)
-vim.keymap.set('n', '<C-v>t', ':vsplit | terminal<CR>', { noremap = true, silent = true })
 
